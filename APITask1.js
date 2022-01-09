3e964d817e36430b79aabd473f0d0ce33acc92d6
Madhavi Latha Yayavaram <lathayvm@gmail.com>
2022-01-09 17:40:23 +0530

    no errors

--- APITask1.js
+++ APITask1.js
@@ -1,27 +1,26 @@
-// FreeDictionary API
-async function getWord() {
-    const data = await 
-    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/hello" , {method:"GET"});
-    const description=await data.json();
-    return description;
+async function getWord(word) {
+  const data = await 
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}` , { method:"GET" });
+  const description=await data.json();
+  return description;
+}
+
+
+  async function displayInfo() {
+    let val = document.getElementById('wordinput').value
+  const wordinfo = [] = await getWord(val);
+      console.log (wordinfo);
+      console.log (wordinfo[0].word);
+    document.getElementById('origin').innerHTML = wordinfo[0].origin
+  const wd = document.getElementById("lists");
+  wd.innerHTML = ''
+  /* get the meanings of the word from the api */
+  let meanings = wordinfo[0].meanings;
+  for(let i = 0; i< meanings.length; i++) {
+    let li = document.createElement('li')
+    li.innerHTML = meanings[i].definitions[0].definition;
+    console.log(li)
+    wd.appendChild(li)
   }
-  
-  
-    async function displayInfo() {
-    const wordinfo = [] =await getWord();
-        console.log (wordinfo);
-        console.log (wordinfo[0].word);
 
-    const wd = document.getElementById("l1");
-    wd.innerHTML = "<p>"+wordinfo[0].word+"</p>";
-    document.body.append(wd);
-
-    const wd2 = document.createElement ("h2");
-    wd2.innerHTML="<p>"+"Hi Madhavi"+"</p>";
-    document.body.append(wd2);
-  
-  } 
-  
-  displayInfo();
-  
-  
+} 
